9e38a289d0ff17e90879229fa437413e73d2ff02
shamil <shamxl@pm.me>
2024-06-15 21:06:27 +0530

    Fixed: Button status not updating correctly
    Pin status remained 'off' even when set to 'on'. This issue occurred because the device's initial registration set the pin status to 'off', which was not updated in the list when requested by the frontend.

--- src/utils/device.cpp
+++ src/utils/device.cpp
@@ -4,16 +4,22 @@
 JsonDocument jsdoc;
 JsonArray Devices = jsdoc.createNestedArray("devices");
 
+
 void register_device (char* name, int pin) {
   pinMode(pin, OUTPUT);
   digitalWrite(pin, HIGH);
   JsonObject device = Devices.createNestedObject();
   device["name"] = name;
   device["pin"] = String(pin);
-  device["high"] = (digitalRead(pin) == HIGH) ? true : false;
+  device["high"] = true;
 }
 
 void device_handler() {
+  for (int i = 0; i < Devices.size(); i++) {
+    JsonObject current_device = Devices[i];
+
+    current_device["high"] = (digitalRead(atoi(current_device["pin"]))) ? true : false;
+  }
   String serialized_json;
   serializeJson(jsdoc, serialized_json);
   server.send(200, "application/json", serialized_json);
